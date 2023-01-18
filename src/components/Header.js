6200b628e72241616b245ed86214360706f44e21
maksat-01 <maksatarykbaiuulu@gmail.com>
2023-01-18 23:37:20 +0600

    initial

--- src/components/Header.js
+++ src/components/Header.js
@@ -40,7 +40,6 @@ const Header = () => {
                         </div>
                     </div>
                 </div>
-                <div className="bg-amber-400">Maksat</div>
                 <div className="w-[70px] header-general flex ">
                     <div  className="flex justify-center items-center absolute top-[50%] m-4">
                         <div onClick={() => setClick(!click)}  className="header-bg cursor-pointer  bg-white fixed  min-h-full w-[70px] h-full left-0 top-0 flex justify-center items-center flex-col">
